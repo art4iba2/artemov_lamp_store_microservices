247db283d3c19007357f87dee04557b36fc826e2
G Art <agg0073@gmail.com>
2026-05-25 00:49:19 +0400

    front + back

--- services/product_service/app/core/config.py
+++ services/product_service/app/core/config.py
@@ -1,8 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     app_name: str = "Product Service"
     database_url: str
+    frontend_origins: str = "http://localhost:5173,http://127.0.0.1:5173"
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
 
 settings = Settings()
